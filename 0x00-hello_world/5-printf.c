8860efa618569493007349f7e4bec07219175a03
vickram odero <vickramodero6@gmail.com>
2023-07-14 23:21:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - main entry point of code
+*Return: always return 0 (success)
+*/
+int main(void)
+{
+	printf("%s, with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
