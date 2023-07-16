ca29ac8d4d02666bd2f398cbc5c645e8079f4625
vickram odero <vickramodero6@gmail.com>
2023-07-16 01:10:40 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main -main entry point
+*Description: -prints all alphabets except e and o
+*Return: alwaysd return 0
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 && n == 113)
+		{
+			putchar(n);
+			n++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
