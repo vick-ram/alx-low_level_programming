0545a8b2bdff61f09859fe6d22090648f4c150e4
vickram odero <vickramodero6@gmail.com>
2023-07-16 01:26:46 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - main entry point
+  *Description: prints number 0 to 9
+  *Return: always return 0
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n <=9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
