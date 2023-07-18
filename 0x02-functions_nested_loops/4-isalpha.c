e40419eafbd0ff76d236020dfb3d82d54a0bd9a7
vickram odero <vickramodero6@gmail.com>
2023-07-18 08:06:45 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,15 +6,13 @@
   */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-		if (c >= 65 && c <= 90)
-		{
-			return (1);
-		}
+		return (1);
 	}
 	else
 	{
 		return (0);
 	}
+	_putchar('\n');
 }
