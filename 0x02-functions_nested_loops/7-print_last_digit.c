0f927467c085b5dcc2fcf0f86806121faaca0407
vickram odero <vickramodero6@gmail.com>
2023-07-18 09:37:12 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,15 @@ int print_last_digit(int n)
 {
 	int ld = (n % 10);
 
-	return (ld);
+	if (ld < 0)
+	{
+		_putchar(-ld + 48);
+		return (-ld);
+	}
+	else
+	{
+		_putchar(ld + 48);
+		return (ld);
+	}
 	_putchar('\n');
 }
