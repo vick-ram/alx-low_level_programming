07d875a0df7830ccb70af52c8b3e51506ac548f5
vickram odero <vickramodero6@gmail.com>
2023-07-18 14:13:06 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *print_to_98 - prints numbers to 98 either way
+  *@n: - an integer type
+  *Return: - return numbers to 98
+  */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d ", i);
+			}
+			else if (i == 98)
+			{
+				printf("%d\n");
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+			{
+				printf("%d, ", j);
+			}
+			else if (j == 98)
+			{
+				printf("%d\n", j);
+			}
+		}
+	}
+}
