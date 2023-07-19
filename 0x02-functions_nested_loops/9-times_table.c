6fd8a59494af64d38071bb6120b243724d34cb83
vickram odero <vickramodero6@gmail.com>
2023-07-18 22:24:10 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,30 +5,31 @@
   */
 void times_table(void)
 {
-	int i, j, k;
+	int i, j, k, l, m;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (j == 0)
+			if (k > 9)
 			{
-				_putchar(k + '0');
-			}
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
+				l = k % 10;
+				m = (k - l) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(m + '0');
+				_putchar(l + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				putchar(k + '0');
 			}
 		}
 		_putchar('\n');
