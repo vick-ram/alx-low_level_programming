b0f472c95314b21183a3327ecb8c9ae854fba43e
vickram odero <vickramodero6@gmail.com>
2023-07-19 02:08:50 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int i;
 	int t1 = 1, t2 = 2;
-	int next = t1 + t2;
+	long long int next = t1 + t2;
 
 	printf("%d, %d,", t1, t2);
 	for (i = 3; i <= 50; i++)
 	{
-		printf("%u,", next);
+		printf("%lu ,", next);
 		t1 = t2;
 		t2 = next;
 		next = t1 + t2;
