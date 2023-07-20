af14e36b84812d747a296bdc1cae2686cc13cfec
vickram odero <vickramodero6@gmail.com>
2023-07-20 14:17:38 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,18 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && a > c)
-    {
+	if (a > b && a > c)
+	{
 	    largest = a;
-    }
-    else if (b > c)
-    {
+	}
+	else if (b > c)
+	{
 	    largest = b;
-    }
-    else
-    {
+	}
+	else
+    	{
 	    largest = c;
-    }
-    return (largest);
+    	}
+
+	return (largest);
 }
