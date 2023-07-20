05914caea1975313ecff7f129a001e440df7309b
vickram odero <vickramodero6@gmail.com>
2023-07-20 04:57:43 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,6 +8,11 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size - i; j++)
@@ -20,5 +25,4 @@ void print_triangle(int size)
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
