116be33379b18eaa741915765842b2aee2ba20e5
vickram odero <vickramodero6@gmail.com>
2023-07-20 04:49:55 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,22 @@ int main(void)
 		if ((n % 3 == 0) && (n % 5 != 0))
 		{
 			printf("Fizz");
-			printf(" ");
+			if (n != 100)
+				printf(" ");
 			continue;
 		}
 		else if ((n % 5 == 0) && (n % 3 != 0))
 		{
 			printf("Buzz");
-			printf(" ");
+			if (n != 100)
+				printf(" ");
 			continue;
 		}
 		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz");
-			printf(" ");
+			if (n != 100)
+				printf(" ");
 			continue;
 		}
 		else
