75b6d6543e3823e2df028025bd92fc492f380c17
vickram odero <vickramodero6@gmail.com>
2023-07-20 11:25:08 -0700

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
