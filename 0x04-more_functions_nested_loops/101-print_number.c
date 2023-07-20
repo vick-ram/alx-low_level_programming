1847043324a5d7fbf6243084dff181ee0b7ed9ef
vickram odero <vickramodero6@gmail.com>
2023-07-20 08:43:10 -0700

    101-print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,52 +1,21 @@
 #include "main.h"
 /**
-  *power - finds powers of ints
-  *@base: - int type
-  *@exp: - int type
-  *Return: - returns result
-  */
-int power(int base, int exp)
-{
-	int result = 1;
-
-	while (exp > 0)
-	{
-		result *= base;
-		exp--;
-	}
-	return (result);
-}
-/**
   *print_number - prints integers using putchar
   *@n: = integer type
   *Return: - returns 0
   */
 void print_number(int n)
 {
-	int numDigits = 0;
-	int temp;
-	int i;
-	int digit;
+	unsigned int i = n;
 
-	if (n == 0)
-	{
-		_putchar(48);
-		return;
-	}
 	if (n < 0)
 	{
 		_putchar(45);
-		n = -n;
+		i = -i;
 	}
-	temp = n;
-	while (temp != 0)
+	if (i / 10)
 	{
-		numDigits++;
-		temp /= 10;
+		print_number(i / 10);
 	}
-	for (i = numDigits - 1; i >= 0; i--)
-	{
-		digit = (n / power(10, i)) % 10;
-		_putchar(48 + digit);
-	}
+	_putchar(i % 10 + '0');
 }
