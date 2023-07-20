406ff3e22ade43b2bfaf577acf0338be1a1a0c08
vickram odero <vickramodero6@gmail.com>
2023-07-20 05:05:13 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,17 +12,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-			{
-				_putchar('0' + j);
-			}
-			else
-			{
-				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
-			}
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
