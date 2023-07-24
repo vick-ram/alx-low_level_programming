194f60ca18f92ae1389146455168f8294e44f5df
vickram odero <vickramodero6@gmail.com>
2023-07-24 12:05:49 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,27 +6,30 @@
   */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int hasSign = 0;
+	int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
 
-	while (*s)
+	while (s[len] != '\0')
 	{
-		if (*s == ' ' || *s == '+' || *s == '-')
-		{
-			hasSign = 1;
-			sign = (*s == '-') ? -1 : 1;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			break;
-		}
-		s++;
+		len++;
 	}
-	while (*s >= '0' && *s <= '9')
+	while (i < len && f == 0)
 	{
-		result = result * 10 + (*s - '0');
-		s++;
+		if (s[i] == '-')
+			++d;
+		if(s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	return ((hasSign) ? (result * sign) : result);
+	if (f == 0)
+		return (0);
+	return (n);
 }
