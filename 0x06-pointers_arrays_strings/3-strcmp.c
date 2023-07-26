b6bb19a1982ba079db312bbe22385c012f0b2de8
vickram odero <vickramodero6@gmail.com>
2023-07-26 09:56:57 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
+		if (s1[i] != s2[i])
 		{
-			return (i);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
-	return (i);
+	return (0);
 }
