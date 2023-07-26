1c37d6f4948d1596ba66f724af95c12cfda3f49c
vickram odero <vickramodero6@gmail.com>
2023-07-26 11:20:52 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,14 +15,17 @@ void printDigit(int digit)
   */
 void print_number(int n)
 {
+	unsigned int num;
+
+	num = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -n;
 	}
-	if (n / 10 != 0)
+	if (num / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	printDigit(n % 10);
+	printDigit(num % 10);
 }
