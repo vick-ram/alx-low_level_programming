769f9fa6d14df8319a5cf5cfe9875eb1327d11ab
vickram odero <vickramodero6@gmail.com>
2023-07-31 01:53:49 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+/**
+  *_strspn - gets the length of prefix substring
+  *@s: - char type param
+  *@accept: - char type param
+  *Return; - returns the number of bytes in the initial segment of of s
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+	int found;
+	char *a;
+
+	while (*s)
+	{
+		found = 0;
+		for (a = accept; *a; a++)
+		{
+			if (*s == *a)
+			{
+				found = 1;
+				break;
+			}
+			accept++;
+		}
+		if (found)
+		{
+			len++;
+			s++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (len);
+}
