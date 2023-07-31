dc389db7b7e2f798ac3876609f30d871694592d4
vickram odero <vickramodero6@gmail.com>
2023-07-31 04:32:33 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,24 +7,22 @@
   */
 char *_strstr(char *haystack, char *needle)
 {
-	char *h = haystack;
-	char *n = needle;
+	char *h;
+	char *n;
 
 	if (*needle == '\0')
-	{
 		return (haystack);
-	}
-	while (*haystack)
+	while (*haystack != '\0')
 	{
-		while (*n && *h == *n)
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *h == *n)
 		{
 			h++;
 			n++;
 		}
 		if (*n == '\0')
-		{
 			return (haystack);
-		}
 		haystack++;
 	}
 	return (0);
