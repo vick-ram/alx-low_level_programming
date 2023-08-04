a4fa75db582bce65f93a8f958eb9795c4c62746b
vickram odero <vickramodero6@gmail.com>
2023-08-04 11:53:17 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,14 +18,17 @@ int main(int argc, char *argv[])
 	}
 	if (cents < 0)
 	{
-		printf("0");
-		printf("\n");
+		printf("0\n");
 		return (0);
 	}
-	coins += cents / 25; cents %= 25;
-	coins += cents / 10; cents %= 10;
-	coins += cents / 5; cents %= 5;
-	coins += cents / 2; cents %= 2;
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
 	coins += cents;
 	printf("%d\n", coins);
 	return (0);
