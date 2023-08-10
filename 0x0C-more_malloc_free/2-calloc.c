df2a17baaeb945f5738a8410890d19d7e219cfd5
vickram odero <vickramodero6@gmail.com>
2023-08-10 13:57:32 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,12 +8,16 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int **arr;
+	void *arr;
+	unsigned int i;
+	unsigned char *b_ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = (int **)malloc(nmemb * size);
-	if (arr == NULL)
-		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr != NULL)
+		b_ptr = (unsigned char *)arr;
+	for (i = 0; i < nmemb * size; i++)
+		b_ptr[i] = 0;
 	return (arr);
 }
