48f08c42e50fd88052e016360f73b81159532b3a
vickram odero <vickramodero6@gmail.com>
2023-08-13 23:56:41 -0700

    dog.h with typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,4 +5,11 @@ struct dog {
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog_t {
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 #endif
