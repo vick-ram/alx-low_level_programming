480c2199ac5102d7d184c10f7e242fe19b5919ec
vickram odero <vickramodero6@gmail.com>
2023-08-14 04:11:51 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,6 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		printf("(nil)\n");
-	d = malloc(sizeof(d));
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
