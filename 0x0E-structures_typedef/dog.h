336546328aebe90a2faa5680b5ef1cc1fbbf54c5
vickram odero <vickramodero6@gmail.com>
2023-08-14 03:54:24 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,32 @@
 #ifndef STRUCT_H
 #define STRUCT_H
-struct dog {
+/**
+*struct dog - structure type of dog
+*@name: -
+*@age: -
+*@owner: -
+*Description: - features of a dog struct
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog_t {
+/**
+  *struct dog_t - structure type of dog
+  *@name: -
+  *@age: -
+  *@owner: -
+  *Description: - features of a dog struct
+  */
+typedef struct dog_t
+{
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
