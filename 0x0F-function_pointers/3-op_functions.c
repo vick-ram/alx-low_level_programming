6dbd7b00eb1c4e5db6aa3611f770d9a6c3192347
vickram odero <vickramodero6@gmail.com>
2023-08-15 23:15:45 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+  *op_add - adds two integers
+  *@a: - first param
+  *@b: - second param
+  *Return: - returns total sum
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  *op_sub - subracts two integers
+  *@a: - first param
+  *@b: - second param
+  *Return: - returns int
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+*op_mul - multiplies two integers
+*@a: - first param
+*@b: - second param
+*Return: - returns multiplied int
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+*op_div - divides two integers
+*@a: - first param
+*@b: - second param
+*Return: - returns divided int
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+*op_mod - finds modulo between two integers
+*@a: - first param
+*@b: - second param
+*Return: - returns remainder int
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
