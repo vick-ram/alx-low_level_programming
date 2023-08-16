188831ae02b860f69320de18d2fa6e5b0d0fd3a6
vickram odero <vickramodero6@gmail.com>
2023-08-16 07:53:00 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
   *print_name - prints a name
   *@name: - char type param
@@ -8,5 +9,6 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
