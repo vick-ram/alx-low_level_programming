74525e2757516960ecc110f7299126d65273e05a
vickram odero <vickramodero6@gmail.com>
2023-08-15 21:24:48 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+  *print_name - prints a name
+  *@name: - char type param
+  *f - function pointer
+  *Return: - returns nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
