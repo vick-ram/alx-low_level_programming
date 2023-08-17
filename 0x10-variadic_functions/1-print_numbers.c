cff9f483b3445e42e819cf6f1366c3e7034b064d
vickram odero <vickramodero6@gmail.com>
2023-08-17 03:50:25 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list arg;
 	int num;
 
-
+	if (separator == NULL)
+		return;
+	if (n == 0)
+		return;
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
