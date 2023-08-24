703d56180898f8d14a449c90b4596c710cf8622a
vickram <vickramodero6@gmail.com>
2023-08-24 15:04:36 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+  *before_main - executes before main function
+  *Return: - returns nothing
+  */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
