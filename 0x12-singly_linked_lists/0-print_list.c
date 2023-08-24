b18ecf5f89ff3f6a237694e5672f5d1381c00633
vickram <vickramodero6@gmail.com>
2023-08-24 13:21:23 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  *print_list -  a struct type to count elements in a list
+  *@h: - a tyrp to define data in a list
+  *Return: - returns the total
+  */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u} %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
