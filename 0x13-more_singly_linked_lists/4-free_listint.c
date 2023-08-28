7f986abf2f5738224db69008f5961476244d0e81
vickram <vickramodero6@gmail.com>
2023-08-28 11:16:25 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+  *free_listint - frees listint_t
+  *@head: - pointer to first node
+  *Return: - returns nothing
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
