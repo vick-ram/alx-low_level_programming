d670e74883151e763163a6ffee4a34c788131806
vickram <vickramodero6@gmail.com>
2023-09-04 13:47:15 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  *reads a text file and prints to the POSIX
+  *read_textfile - reads a text file and prints to the POSIX
   *standard output
   *@filename: - the name of the file
   *@letters: - the letters to be printed
@@ -12,20 +12,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t bytes_read, bytes_written;
 
-	if (filename == NULL)
-		return (0);
 	fd = open(filename, 0_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
 	bytes_read = read(fd, buffer, letters);
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	close(fd);
 	free(buffer);
+	close(fd);
 	return (bytes_written);
 }
