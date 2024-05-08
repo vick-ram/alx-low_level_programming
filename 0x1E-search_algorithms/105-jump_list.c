f2a31ad588dcd29351b4150b206a18f4a2c57cd2
vick-ram <vickramodero6@gmail.com>
2024-05-08 14:39:37 -0700

    jump search in a linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,60 +1,43 @@
 #include "search_algos.h"
-listint_t *jump_forward(listint_t *list, size_t steps);
+
 /**
- * jump_list - Searches for a value in a sorted list of integers
- *             using the Jump search algorithm.
- * @list: A pointer to the head of the list to search.
+ * jump_list - Searches for an algorithm in a sorted singly
+ *             linked list of integers using jump search.
+ * @list: A pointer to the  head of the linked list to search.
  * @size: The number of nodes in the list.
  * @value: The value to search for.
  *
- * Return: A pointer to the first node where the value is located,
- *         or NULL if the value is not present in the list or if head is NULL.
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t jump_step, prev_idx;
-	listint_t *node, *prev;
+	size_t step = 0, jump_step = sqrt(size);
+	listint_t *node, *jump;
 
 	if (list == NULL || size == 0)
 		return (NULL);
 
-	jump_step = sqrt(size);
-	prev_idx = 0;
-	prev = list;
-	node = jump_forward(list, jump_step);
-	while (node != NULL && node->n < value)
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
-		prev = node;
-		prev_idx = node->index;
-		node = jump_forward(node, jump_step);
+		node = jump;
+		for (step += jump_step; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 	}
-	printf("Value found between indexes [%lu] and [%lu]\n", prev_idx,
-	node ? node->index : size - 1);
-	while (prev != NULL && prev->n < value)
-	{
-		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-		if (prev->n == value)
-		return (prev);
-		prev = prev->next;
-	}
-	return (NULL);
-}
 
-/**
- * jump_forward - Jumps forward a specified number of nodes in a linked list.
- * @list: A pointer to the head of the list.
- * @steps: The number of steps to jump forward.
- *
- * Return: A pointer to the node after jumping forward, or NULL if the end
- *         of the list is reached before completing all steps.
- */
-listint_t *jump_forward(listint_t *list, size_t steps)
-{
-	size_t i;
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-	for (i = 0; i < steps && list != NULL; i++)
-		list = list->next;
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
-	return (list);
+	return (node->n == value ? node : NULL);
 }
